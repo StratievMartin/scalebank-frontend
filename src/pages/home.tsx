0d545d3f6773be44a95844c3b2f34529b414b436
Martin Stratiev <martinstratiev1@gmail.com>
2023-05-10 19:51:05 +0300

    initial setup commit fixes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,46 +1,13 @@
-import { Box } from "@chakra-ui/react"
-
+import { Box } from '@chakra-ui/react'
+import { mock } from '../mock-data/mock.js'
 export default function Home() {
-  const mock = {
-    id: 1,
-    bank_name: 'XYZ Bank',
-    account_holder: 'John Doe',
-    account_number: '1234567890',
-    statement_period: '1st January 2023 - 31st January 2023',
-    transactions: [
-      {
-        id: 1,
-        date: '1/1/2023',
-        description: 'Opening Balance',
-        deposit: null,
-        withdrawal: 0,
-        balance: 10000,
-      },
-      {
-        id: 2,
-        date: '1/5/2023',
-        description: 'Salary',
-        deposit: 5000,
-        withdrawal: 0,
-        balance: 15000,
-      },
-      {
-        id: 3,
-        date: '1/10/2023',
-        description: 'Grocery Store',
-        deposit: null,
-        withdrawal: 500,
-        balance: 14500,
-      },
-      {
-        id: 4,
-        date: '1/15/2023',
-        description: 'ATM Withdrawal',
-        deposit: null,
-        withdrawal: 200,
-        balance: 14300,
-      },
-    ],
+  interface Transaction {
+    id: number
+    date: string
+    description: string
+    deposit: number | null
+    withdrawal: number | null
+    balance: number
   }
 
   return (
@@ -49,22 +16,17 @@ export default function Home() {
       <h2>{mock.account_holder}</h2>
       <h2>{mock.account_number}</h2>
       <h2>{mock.statement_period}</h2>
-      <p>
-        {mock.transactions.map((transaction) => {
+      <Box>
+        {mock.transactions.map((transaction: Transaction) => {
           return (
-            <Box
-              border="1px solid red"
-              p="20px"
-              m="20px"
-              key={transaction.id}
-            >
+            <Box border="1px solid red" p="20px" m="20px" key={transaction.id}>
               <p>{transaction?.deposit}</p>
               <p>{transaction?.description}</p>
               <p>{transaction?.date}</p>
             </Box>
           )
         })}
-      </p>
+      </Box>
     </>
   )
 }
