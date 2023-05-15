88d8a4056799dc7374827d45a142d9c7fd71c897
Martin Stratiev <martinstratiev1@gmail.com>
2023-05-15 10:48:41 +0300

    displaying data from uploaded csv file

---
+++ src/components/CsvFormUpload.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Papa from 'papaparse'
+import UiTable from './Table'
+
+type CsvDataType = {
+  data: string[][]
+  errors: any[]
+  meta: {
+    delimiter: string
+    linebreak: string
+  }
+}
+
+export default function CSVUploader() {
+  const [csvData, setCsvData] = useState<string[][]>([])
+
+  const handleCsvUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0]
+
+
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        complete: (result: CsvDataType) => {
+          console.log(result.data)
+          setCsvData(result.data)
+        },
+      })
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleCsvUpload} />
+      {csvData.length > 0 && (
+        <UiTable tableName="CSV Data" head={csvData[0]} body={csvData}></UiTable>
+      )}
+    </div>
+  )
+}
