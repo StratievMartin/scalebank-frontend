d3eb2a6ab835e66d533bb349bb681c66e94da03f
Dimitur <omgweee74@gmail.com>
2023-05-18 17:36:20 +0300

    user-profile-functionality

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,72 +1,47 @@
 import { Container, Input, Button} from '@chakra-ui/react'
 import { useState } from 'react'
 
-export default function profile() {
-  const [userValue,setUserValue] = useState("")
-  const [passValue,setPassValue] = useState("")
-  const [mailValue,setMailValue] = useState("")
-  const [dateValue,setDateValue] = useState("")
-  const [ageValue,setAgeValue] = useState("")
+const initialValues = {
+  username: "",
+  password: "",
+  mail: "",
+  date: "",
+  age: "",
+};
+
+export default function Form() {
+  const [values, setValues] = useState(initialValues)
+
+  const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
   const alertClick = () => {
-    alert (userValue)
-    alert (passValue)
-    alert (mailValue)
-    alert (dateValue)
-    alert (ageValue)
+    alert (values.username)
+    alert (values.password)
+    alert (values.mail)
+    alert (values.date)
+    alert (values.age)
   }
-
-  const logClick = () => {
-    console.log (userValue)
-    console.log (passValue)
-    console.log (mailValue)
-    console.log (dateValue)
-    console.log (ageValue)
-  }
-
-  const deleteClick = () => {
-    setUserValue("") 
-    setPassValue("") 
-    setMailValue("") 
-    setAgeValue("") 
-    setDateValue("") 
-  }
-
-  const userChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserValue(event.target.value)
-  }
-
-  const passChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassValue(event.target.value)
-  }
-
-  const mailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setMailValue(event.target.value)
-  }
-
-  const dateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDateValue(event.target.value)
-  }
-
-  const ageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAgeValue(event.target.value)
-  }
-
+  
   return (
     <>
       <Container>
         Welcome User
         <div>
-          <Input onChange={userChange} value={userValue} placeholder="Username" />
-          <Input onChange={passChange} value={passValue} placeholder="Password" />
-          <Input onChange={mailChange} value={mailValue} placeholder="Email" />
-          <Input onChange={dateChange} value={dateValue} placeholder="Date" type="date" />
-          <Input onChange={ageChange} value={ageValue} placeholder="Age" type="number" />
+          <Input onChange={handleInputChange} value={values.username} name='username' placeholder="Username" />
+          <Input onChange={handleInputChange} value={values.password} name='password' placeholder="Password" />
+          <Input onChange={handleInputChange} value={values.mail} name='mail' placeholder="Email" />
+          <Input onChange={handleInputChange} value={values.date} name='date' placeholder="Date" type="date" />
+          <Input onChange={handleInputChange} value={values.age} name='age' placeholder="Age" type="number" />
         </div>
         <div>
-          <Button onClick={logClick} colorScheme="blue">Add</Button>
           <Button onClick={alertClick} colorScheme="green">Update</Button>
-          <Button onClick={deleteClick} colorScheme="red">Delete</Button>
         </div>
         <div>
           <Button colorScheme="red">Logout</Button>
